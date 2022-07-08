f51f16e2c06bf54cfcd4cee02a7435026074ff3a
Woodwindy <odunsisamuel876@gmail.com>
2022-07-07 20:15:15 -0700

    My commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,16 +8,33 @@
  * @n: data to be added.
  * Return: address of new node or NULL if it failed.
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-
 {
-	unsigned int i;
+	dlistint_t *newnode, *temp;
+	unsigned int counter;
 
-	for (i = 0; i < index && head != NULL; i++)
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	newnode->prev = NULL;
+	if (idx == 0)
 	{
-		head = head->next;
+		*h = newnode;
+		return (newnode);
 	}
+	temp = *h;
+	for (counter = 0; counter < idx - 1; counter++)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+	newnode->next = temp->next;
+	temp->next->prev = newnode;
+	temp->next = newnode;
+	newnode->prev = temp;
 
-	return (head);
+	return (newnode);
 }
