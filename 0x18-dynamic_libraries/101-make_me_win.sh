256145343b9d485b05a41680c937c1b32d86462a
Woodwindy <odunsisamuel876@gmail.com>
2022-07-16 20:14:30 -0700

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Woodwindy/alx-low_level_programming/tree/master/0x18-dynamic_libraries/winlib.so
+wget -P /tmp https://github.com/Woodwindy/alx-low_level_programming/blob/master/0x18-dynamic_libraries/winlib.so
 export LD_PRELOAD=/tmp/winlib.so
