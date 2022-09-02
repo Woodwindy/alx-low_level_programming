e6eec8ebda211b0325e98c04d2efe030d217ccd2
Woodwindy <odunsisamuel876@gmail.com>
2022-09-01 18:41:36 -0700

    A function that print its own name even when its name gets changed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /** 
- * main - prints its name, followed by a  new line
+ * This program prints its own name even when its name is changed.
+ * main - prints its name, followed by a  new line.
  * @argc: argument counter.
- * @argv: argument vector
+ * @argv: argument vector.
  * Return: zero.
  **/
 
